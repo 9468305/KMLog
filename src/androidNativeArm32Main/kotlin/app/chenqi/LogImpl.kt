991b0ed7ad8055d342f9b8ba73b2ed278b9e3ea9
chenqi <chenqib@Ctrip.com>
2019-05-29 15:06:41 +0800

    Update AndroidNativeArm32

--- src/androidNativeArm32Main/kotlin/app/chenqi/LogImpl.kt
+++ src/androidNativeArm32Main/kotlin/app/chenqi/LogImpl.kt
@@ -4,10 +4,10 @@ import platform.android.*
 
 actual object Log {
     actual fun isLoggable(tag: String, level: Level): Boolean  = level >= Level.INFO
-    actual fun v(tag: String, msg: String) {__android_log_print(ANDROID_LOG_VERBOSE.toInt(), "Tag", "%s", "msg")}
-    actual fun d(tag: String, msg: String) {__android_log_print(ANDROID_LOG_DEBUG.toInt(), "Tag", "%s", "msg")}
-    actual fun i(tag: String, msg: String) { __android_log_print(ANDROID_LOG_INFO.toInt(), "Tag", "%s", "msg")}
-    actual fun w(tag: String, msg: String) {__android_log_print(ANDROID_LOG_WARN.toInt(), "Tag", "%s", "msg")}
-    actual fun e(tag: String, msg: String) {__android_log_print(ANDROID_LOG_ERROR.toInt(), "Tag", "%s", "msg")}
-    actual fun wtf(tag: String, msg: String) { __android_log_print(ANDROID_LOG_FATAL.toInt(), "Tag", "%s", "msg")}
+    actual fun v(tag: String, msg: String) {__android_log_print(ANDROID_LOG_VERBOSE.toInt(), tag, "%s", msg)}
+    actual fun d(tag: String, msg: String) {__android_log_print(ANDROID_LOG_DEBUG.toInt(), tag, "%s", msg)}
+    actual fun i(tag: String, msg: String) { __android_log_print(ANDROID_LOG_INFO.toInt(), tag, "%s", msg)}
+    actual fun w(tag: String, msg: String) {__android_log_print(ANDROID_LOG_WARN.toInt(), tag, "%s", msg)}
+    actual fun e(tag: String, msg: String) {__android_log_print(ANDROID_LOG_ERROR.toInt(), tag, "%s", msg)}
+    actual fun wtf(tag: String, msg: String) { __android_log_print(ANDROID_LOG_FATAL.toInt(), tag, "%s", msg)}
 }
